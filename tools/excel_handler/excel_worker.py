e91ec2ae49bccc8872d7f5c7249cf549340bd209
BrenC222 <brencdev.io@gmail.com>
2023-11-28 15:55:36 -0600

    Handled meta and frame data sheets separately CHORE: work on fighter class

--- tools/excel_handler/excel_worker.py
+++ tools/excel_handler/excel_worker.py
@@ -3,13 +3,12 @@ import json
 import os
 
 class ExcelSheetWorker:
-    def __init__(self, excel_file_path, sheet_name):
-        self.df = pd.read_excel(excel_file_path, sheet_name, skiprows=1)
+    def __init__(self, sheet_name, sheet_data):
         self.sheet_name = sheet_name
-
+        self.sheet_data = sheet_data
+        
     def to_json(self):
-        json_data = self.df.to_dict(orient='records')
-        return {self.sheet_name: json_data}
+        return {self.sheet_name: self.sheet_data}
 
     def _save_to_json_file(self, file_path, indent=2):
         with open(file_path, 'w', encoding='utf-8', errors='replace') as file:
@@ -19,23 +18,47 @@ class ExcelSheetWorker:
     def _get_sheet_names(excel_file_path):
         try:
             with pd.ExcelFile(excel_file_path) as excel_file:
-                sheet_names = [sheet_name for sheet_name in excel_file.sheet_names if not sheet_name.endswith('-meta')]
+                sheet_names = [sheet_name for sheet_name in excel_file.sheet_names]
             return sheet_names
         except Exception as e:
             print(f"Error reading excel file: {e}")
             return []
+
+    @staticmethod
+    def _process_frame_sheet(excel_file_path, sheet_name):
+        df = pd.read_excel(excel_file_path, sheet_name, skiprows=1)
+        sheet_data = df.to_dict(orient='records')
+        return sheet_data
+
+    @staticmethod
+    def _process_meta_sheet(excel_file_path, sheet_name):
+        df = pd.read_excel(excel_file_path, sheet_name)
+        sheet_data = df.to_dict(orient='records')
+        return sheet_data
         
     @classmethod
     def excel_to_json(cls, file_path):
-        sheet_names = cls._get_sheet_names(file_path)
-        sheet_data = {}
-        for sheet_name in sheet_names:
+        tsheet_name = cls._get_sheet_names(file_path)
+        tsheet_data = {}
+        for sheet_name in tsheet_name:
             print(f"Working on {sheet_name}")
-            sheet_data[sheet_name] = cls(file_path, sheet_name)
+            if sheet_name.endswith('-meta'):
+                sheet_data = cls._process_meta_sheet(file_path, sheet_name)
+                tsheet_data[sheet_name] = cls(sheet_name, sheet_data)
+            else:
+                sheet_data = cls._process_frame_sheet(file_path, sheet_name)
+                tsheet_data[sheet_name] = cls(sheet_name, sheet_data)
+                
             print("Done...")
             
-        for sheet_name, data in sheet_data.items():
-            file_path = f"data/frame/{sheet_name}_output.json"
-            print(f"Writing file {file_path}")
-            data._save_to_json_file(file_path)
+        for sheet_name, sheet_data in tsheet_data.items():
+            if sheet_name.endswith('-meta'):
+                output_path = f"data/meta/{sheet_name}_output.json"
+                print(f"Writing file {output_path}")
+                sheet_data._save_to_json_file(output_path)
+            else:
+                output_path = f"data/frame/{sheet_name}_output.json"
+                print(f"Writing file {output_path}")
+                sheet_data._save_to_json_file(output_path)
+
             print("Done...")
